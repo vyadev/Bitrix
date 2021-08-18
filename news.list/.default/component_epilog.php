39b01b39f0cc7d71f46d6a0d0cb4c394941e18ea
gitbitsl <sl201@inbox.ru>
2021-08-18 12:16:38 +0500

    Delete news.list/.default directory

--- news.list/.default/component_epilog.php
+++
@@ -1,4 +0,0 @@
-<?
-	if (isset($arResult["DATE_FIRST_NEWS"])) {
-			$APPLICATION->SetPageProperty("specialdate", $arResult["DATE_FIRST_NEWS"]);
-	}
--- news.list/.default/result_modifier.php
+++
@@ -1,5 +0,0 @@
-<?
-	if ($arParams["SPECIALDATE"] == "Y") {
-		$arResult["DATE_FIRST_NEWS"] = $arResult["ITEMS"][0]["ACTIVE_FROM"];
-	}
-	$this->__component->SetResultCacheKeys(array("DATE_FIRST_NEWS"));
