6e6b24e203fbfbef737c69585810487beec14a1a
slava <sl201@inbox.ru>
2021-08-18 11:14:29 +0500

    add complexcomp

---
+++ complexcomp/.description.php
@@ -0,0 +1,13 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+$arComponentDescription = array(
+	"NAME" => GetMessage("IBLOCK_PHOTO_NAME"),
+	"DESCRIPTION" => GetMessage("IBLOCK_PHOTO_DESCRIPTION"),
+	"ICON" => "/images/photo.gif",
+	"COMPLEX" => "Y",
+	"PATH" => array(
+		"ID" => "ex2simple",
+		"NAME" => GetMessage("IBLOCK_PHOTO_SECTION"),
+	),
+);
+?>
---
+++ complexcomp/templates/.default/exampage.php
@@ -0,0 +1,7 @@
+<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?>
+<?
+//Значения переменных:
+// echo "<pre>"; var_dump($arResult); echo "</pre>";
+echo "PARAM1 = ". $arResult["VARIABLES"]["PARAM1"]. "<br>";
+echo "PARAM2 = ". $arResult["VARIABLES"]["PARAM2"];
+?>
