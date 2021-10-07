aa3c16cbe6fd72b2708264afd79ffd85fa5cf152
vyadev <sl201@inbox.ru>
2021-10-07 15:59:12 +0500

    Delete local directory

--- local/templates/exam1/lang/ru/description.php
+++
@@ -1,4 +0,0 @@
-<?
-$MESS ['T_DEFAULT_DESC_NAME'] = "Экзамен N1";
-$MESS ['T_DEFAULT_DESC_DESC'] = "Экзамен N1";
-?>
--- local/templates/exam1/page_templates/.content.php
+++
@@ -1,4 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();?><?
-IncludeTemplateLangFile(__FILE__);
-$TEMPLATE["inner.php"] = Array("name"=>GetMessage("inner"), "sort"=>1);
-?>
